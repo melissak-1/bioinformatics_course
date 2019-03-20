c28777828e068961b89ab47f6a1744536b7bf832
melissak-1 <45179283+melissak-1@users.noreply.github.com>
2019-03-20 14:26:29 +0000

    Update my-script.sh

--- scripts/my-script.sh
+++ scripts/my-script.sh
@@ -1,1 +1,15 @@
 #\!/usr/bin/env bash
+mkdir -p analysis docs data
+for my_directory in scripts analysis docs data;do
+
+  touch ${my_directory}/README.md
+
+  echo “# ${my_directory}” >> ${my_directory}/README.md
+
+done
+chmod +x my-script.sh
+git add .
+
+git commit -m "not tested: updated script to make empty dirs and README.md
+
+git push
